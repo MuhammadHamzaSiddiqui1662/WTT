33a159769a960a65790c57cbd61ac5b336d10c59
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-09 02:41:26 +0500

    feat: setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+  ReactPayPalScriptOptions,
+} from "@paypal/react-paypal-js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styles from "./App.module.scss";
+import { Body } from "./Body";
+import { ROUTES } from "./config/routesConfig";
+// @ts-ignore
+import * as AOS from "aos";
+import "aos/dist/aos.css";
+import { initialOptions } from "./config/paypalConfig";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <PayPalScriptProvider options={initialOptions as ReactPayPalScriptOptions}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}>
+              {ROUTES.map((route) => (
+                <Route key={route.url} path={route.url} Component={route.component} />
+              ))}
+              <Route path="*" element={<h3 style={{ color: "white" }}>Not Found</h3>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PayPalScriptProvider>
     </div>
   );
 }
 
 export default App;
+
