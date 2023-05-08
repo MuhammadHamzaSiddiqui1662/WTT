ddc416a9811da598dfd7c5bd984407cde97016a0
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-09 02:45:55 +0500

    Feat: Tools Card comp created

---
+++ src/components/ToolsCard/ToolsCard.tsx
@@ -0,0 +1,58 @@
+import { FC } from "react";
+import styles from "./ToolsCard.module.scss";
+import { Button } from "../Button/Button";
+
+interface IProps {
+  title: string;
+  image: string;
+  alt: string;
+  price: number;
+  animation?: string;
+  delay?: number;
+  duration?: number;
+  anchor?: string;
+}
+
+export const ToolsCard: FC<IProps> = ({
+  title,
+  image,
+  alt,
+  price,
+  animation = "fade-left",
+  delay,
+  duration,
+  anchor = "top-bottom",
+}) => {
+  return (
+    <div
+      className={styles.container}
+      data-aos={animation}
+      data-aos-delay={delay}
+      data-aos-duration={duration}
+      data-aos-anchor-placement={anchor}
+    >
+      <img
+        className={styles.image}
+        src={image}
+        alt={alt}
+        data-aos="zoom-in"
+        data-aos-delay="200"
+        data-aos-duration="500"
+        data-aos-anchor-placement="top-bottom"
+      />
+      <div className={styles.caption}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.price}>Price: ${price}</p>
+        <Button
+          fullwidth={true}
+          data-aos="fade-up"
+          data-aos-delay="200"
+          data-aos-duration="500"
+          data-aos-anchor-placement="top"
+        >
+          Add to Basket
+        </Button>
+      </div>
+    </div>
+  );
+};
