492de322e99e1e6ad3223db1b3b1af8eb6445ddb
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-09 02:49:33 +0500

    feat: Notes page created

---
+++ src/pages/Notes/Notes.tsx
@@ -0,0 +1,62 @@
+import { FC, useState } from "react";
+import styles from "./Notes.module.scss";
+import { PageHeading } from "src/components/PageHeading/PageHeading";
+import { SectionHeading } from "src/components/SectionHeading/SectionHeading";
+import { Page } from "src/components/Page/Page";
+import { NotesCard } from "src/components/NotesCard/NotesCard";
+import graphImage from "src/assets/images/exampleGraph.webp";
+
+export const Notes: FC = () => {
+  return (
+    <Page>
+      <div className={styles.container}>
+        <PageHeading heading={"My Notes"} description="" />
+        <section className={styles.section}>
+          <SectionHeading heading="Morning prep" />
+          <div className={styles.notesCards}>
+            <NotesCard
+              title="Morning Notes"
+              description={
+                "If you are committed to this business, discipline is key. Build yourself a routine. Stick to it. Interpreting the morning levels, prep for the day (knowing in advance where my trades could be) and sharing my morning notes is my daily routine. BE DISCIPLINED!"
+              }
+              image={graphImage}
+              alt="abc graph"
+            />
+            <NotesCard
+              title="Morning Notes"
+              description={
+                "If you are committed to this business, discipline is key. Build yourself a routine. Stick to it. Interpreting the morning levels, prep for the day (knowing in advance where my trades could be) and sharing my morning notes is my daily routine. BE DISCIPLINED!"
+              }
+              image={graphImage}
+              alt="abc graph"
+            />
+            <NotesCard
+              title="Morning Notes"
+              description={
+                "If you are committed to this business, discipline is key. Build yourself a routine. Stick to it. Interpreting the morning levels, prep for the day (knowing in advance where my trades could be) and sharing my morning notes is my daily routine. BE DISCIPLINED!"
+              }
+              image={graphImage}
+              alt="abc graph"
+            />
+            <NotesCard
+              title="Morning Notes"
+              description={
+                "If you are committed to this business, discipline is key. Build yourself a routine. Stick to it. Interpreting the morning levels, prep for the day (knowing in advance where my trades could be) and sharing my morning notes is my daily routine. BE DISCIPLINED!"
+              }
+              image={graphImage}
+              alt="abc graph"
+            />
+            <NotesCard
+              title="Morning Notes"
+              description={
+                "If you are committed to this business, discipline is key. Build yourself a routine. Stick to it. Interpreting the morning levels, prep for the day (knowing in advance where my trades could be) and sharing my morning notes is my daily routine. BE DISCIPLINED!"
+              }
+              image={graphImage}
+              alt="abc graph"
+            />
+          </div>
+        </section>
+      </div>
+    </Page>
+  );
+};
