192fce84866f8980494be249f1e9a004c83b3d04
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-09 02:46:41 +0500

    feat: Paypal config added

---
+++ src/config/paypalConfig.ts
@@ -0,0 +1,9 @@
+import { ReactPayPalScriptOptions } from "@paypal/react-paypal-js";
+
+export const initialOptions: ReactPayPalScriptOptions = {
+  "client-id": "AWyXg0IbrcDMpuB26BdWh5RHSGIP6haXpdLVNoLfeoWxggZd87lS-AjL5N7G8eHHuLP7TXg-4YJ-OLGI",
+  currency: "USD",
+  intent: "subscription",
+  vault: true,
+  "data-react-paypal-script-id": "abc",
+};
