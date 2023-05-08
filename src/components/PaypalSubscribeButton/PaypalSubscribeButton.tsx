005fde2d6dfcfa33c7a9bc10c0bcb33ba9ec55b8
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-09 02:47:05 +0500

    feat: paypal subscription Button created

---
+++ src/components/PaypalSubscribeButton/PaypalSubscribeButton.tsx
@@ -0,0 +1,30 @@
+import { PayPalButtons } from "@paypal/react-paypal-js";
+
+export const PaypalSubscribeButton = () => {
+  const handleApprove = async (data: any, action: any) => {
+    console.log("running handleApprove...");
+    console.log("action", action);
+    console.log("///////////////////////////////////////////////////////");
+    console.log("data", data);
+  };
+  return (
+    <PayPalButtons
+      createSubscription={(data, actions) => {
+        return actions.subscription
+          .create({
+            plan_id: "P-3RX065706M3469222L5IFM4I",
+          })
+          .then((orderId) => {
+            // Your code here after create the order
+            console.log("order Id: ", orderId);
+            return orderId;
+          });
+      }}
+      onApprove={handleApprove}
+      onError={(err) => console.log("error", err)}
+      style={{
+        label: "subscribe",
+      }}
+    />
+  );
+};
