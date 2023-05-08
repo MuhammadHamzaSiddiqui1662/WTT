c129ce863aa98ab70364a7b36ee2aa4448c79a55
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-09 02:42:04 +0500

    feat: button comp created

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import { FC, ReactNode } from "react";
+import styles from "./Button.module.scss";
+
+interface IProps {
+  children: ReactNode;
+  type?: "button" | "submit" | "reset" | undefined;
+  className?: CSSStyleRule | string;
+  fullwidth?: boolean;
+  onClick?: (e: any) => void;
+}
+
+export const Button: FC<IProps> = ({ children, className, type, fullwidth = false, onClick, ...props }) => {
+  return (
+    <div className={styles.container} {...props}>
+      <button
+        className={`${styles.button} ${fullwidth && styles.fullwidth} ${className}`}
+        type={type}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
