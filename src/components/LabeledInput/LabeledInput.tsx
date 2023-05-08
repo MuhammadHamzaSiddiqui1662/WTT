b61ffda057a6ea9f20912e9cf03fa37f6a037636
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-09 02:44:19 +0500

    feat: LabeledInput comp created

---
+++ src/components/LabeledInput/LabeledInput.tsx
@@ -0,0 +1,23 @@
+import { FC, ChangeEvent } from "react";
+import styles from "./LabeledInput.module.scss";
+
+interface IProps {
+  label: string;
+  name?: string;
+  placeholder?: string;
+  type?: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  default?: string;
+}
+
+export const LabeledInput: FC<IProps> = ({ label, name, ...props }) => {
+  return (
+    <div className={styles.container}>
+      <label className={styles.label} htmlFor={name || label}>
+        {label}:
+      </label>
+      <input className={styles.input} type="text" id={name || label} name={name || label} {...props} />
+    </div>
+  );
+};
