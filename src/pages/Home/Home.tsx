dd3548bed16296f98b23fcf27c81c060495604a7
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-09 02:48:25 +0500

    feat: Home page created

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+import styles from "./Home.module.scss";
+import { Button } from "src/components/Button/Button";
+import { Page } from "src/components/Page/Page";
+
+export const Home: FC = () => {
+  return (
+    <Page>
+      <div className={styles.container}>
+        <div className={styles.rightSide}>
+          <h1
+            className={styles.heading}
+            data-aos="fade-right"
+            data-aos-delay="400"
+            data-aos-duration="1000"
+            data-aos-anchor-placement="top-bottom"
+          >
+            Bringing your trading experience to a higher level, using my edge.
+          </h1>
+          <p
+            className={styles.description}
+            data-aos="fade-left"
+            data-aos-delay="600"
+            data-aos-duration="1000"
+            data-aos-anchor-placement="top-bottom"
+          >
+            Trading is a challenging field, and having an edge can make all the difference. Stay one step ahead of the
+            game by using proven approach and insights to navigate the dynamic world of trading. Let my edge sharpen
+            your decision-making process, increase your profit potential, and pave the way to success. Don't just trade,
+            stand out with this edge that I am willing to share.
+          </p>
+          <Button
+            data-aos="fade-up"
+            data-aos-delay="500"
+            data-aos-duration="1000"
+            data-aos-anchor-placement="top-bottom"
+          >
+            Get Started
+          </Button>
+        </div>
+      </div>
+    </Page>
+  );
+};
