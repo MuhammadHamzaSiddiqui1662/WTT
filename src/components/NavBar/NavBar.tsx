ee447dcec01bc310c021fe8e4c876755cc6466f5
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-12 02:41:12 +0500

    feat: burger menu for mobile view added

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,10 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "src/assets/images/logo.png";
 import styles from "./NavBar.module.scss";
 import { ROUTES } from "src/config/routesConfig";
+import { RxHamburgerMenu } from "react-icons/rx";
+import OutsideClickHandler from "react-outside-click-handler";
 
 interface IProps {
   className?: CSSRule | string;
@@ -10,6 +12,7 @@ interface IProps {
 
 export const NavBar: FC<IProps> = ({ className }) => {
   const { pathname } = useLocation();
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
     <div className={styles.wrapper}>
       <div className={styles.container + " " + className}>
@@ -21,7 +24,26 @@ export const NavBar: FC<IProps> = ({ className }) => {
             </li>
           ))}
         </ul>
+        <RxHamburgerMenu className={styles.menuIcon} size={24} onClick={() => setMenuOpened(true)} />
       </div>
+      {menuOpened && (
+        <div className={styles.burgerWrapper}>
+          <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
+            <div className={styles.burgerMenu}>
+              <h2 className={styles.heading}>Menu</h2>
+              <ul className={styles.routes}>
+                {ROUTES.map((route) => (
+                  <li key={route.url} className={pathname === route.url ? styles.active : ""}>
+                    <Link to={route.url} onClick={() => setMenuOpened(false)}>
+                      {route.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </OutsideClickHandler>
+        </div>
+      )}
     </div>
   );
 };
