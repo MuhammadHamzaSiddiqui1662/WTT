2381ee2ab2f3433e22f9921ceea286eef5524e8a
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-12 12:30:48 +0500

    feat: footer mobile view + animation delay removed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,22 +7,10 @@ export const Footer: FC = () => {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.risk}>
-          <p
-            className={styles.heading}
-            data-aos="fade-up"
-            data-aos-delay="500"
-            data-aos-duration="700"
-            data-aos-anchor-placement="top"
-          >
+          <p className={styles.heading} data-aos="fade-up" data-aos-duration="700" data-aos-anchor-placement="top">
             RISK DISCLOSURE:
           </p>
-          <p
-            className={styles.description}
-            data-aos="fade-up"
-            data-aos-delay="500"
-            data-aos-duration="700"
-            data-aos-anchor-placement="top"
-          >
+          <p className={styles.description} data-aos="fade-up" data-aos-duration="700" data-aos-anchor-placement="top">
             Futures, forex and stock trading contains substantial risk and is not for every investor. An investor could
             potentially lose all or more than the initial investment. Risk capital is money that can be lost without
             jeopardizing ones financial security or life style. Only risk capital should be used for trading and only
@@ -32,7 +20,7 @@ export const Footer: FC = () => {
           <p
             className={styles.heading}
             data-aos="fade-up"
-            data-aos-delay="600"
+            data-aos-delay="100"
             data-aos-duration="700"
             data-aos-anchor-placement="top"
           >
@@ -41,7 +29,7 @@ export const Footer: FC = () => {
           <p
             className={styles.description}
             data-aos="fade-up"
-            data-aos-delay="600"
+            data-aos-delay="100"
             data-aos-duration="700"
             data-aos-anchor-placement="top"
           >
