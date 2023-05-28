1a4665af20ed91254846352fb190141d7ad8534e
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-05-28 21:06:56 +0500

    test: testing ip lookup

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,9 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./components/NavBar/NavBar";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Footer } from "./components/Footer/Footer";
 
 export const Body: FC = () => {
+  useEffect(() => {
+    fetch("https://extreme-ip-lookup.com/json/")
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("Country: ", response);
+      })
+      .catch(() => {
+        console.log("Request failed");
+      });
+  });
   return (
     <>
       <NavBar />
