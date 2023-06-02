115d10f7423550fe59e9c002dfd86a521f4c99c9
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-06-02 18:41:37 +0500

    feat: forgot password feature added in login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,31 +1,45 @@
-import { FC, useState } from "react";
+import { FC, FormEvent, useEffect, useState } from "react";
 import styles from "./Login.module.scss";
 import { LabeledInput } from "src/components/LabeledInput/LabeledInput";
 import { Button } from "src/components/Button/Button";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "src/firebase";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Login: FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (auth.currentUser) {
+      console.log(auth.currentUser);
+      navigate("/");
+    }
+  }, []);
 
-  const handleSignIn = async (email: string, password: string) => {
+  const handleLogin = async (email: string, password: string) => {
     try {
       const user = await signInWithEmailAndPassword(auth, email, password);
       console.log(user);
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
   };
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    handleLogin(email, password);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.card}>
+      <form className={styles.card} onSubmit={handleSubmit}>
         <h2 className={styles.heading}>Sign In</h2>
         <LabeledInput
           label="Email"
           placeholder="Enter your email..."
+          required={true}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
@@ -33,16 +47,20 @@ export const Login: FC = () => {
           label="Password"
           type="password"
           placeholder="Enter yor password..."
+          required={true}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <p className={styles.signUp}>
+        <p className={styles.text}>
+          Forgot Password? <Link to="/forgot-password">Reset</Link> now
+        </p>
+        <Button className={styles.button} type="submit" fullwidth={true}>
+          Login
+        </Button>
+        <p className={styles.text}>
           Don't have an account? <Link to="/sign-up">Sign Up</Link> now
         </p>
-        <Button className={styles.button} fullwidth={true} onClick={() => handleSignIn(email, password)}>
-          Sign In
-        </Button>
-      </div>
+      </form>
     </div>
   );
 };
