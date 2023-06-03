6391130bc5913037b2724e97edc414f368bf16d8
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-06-04 02:49:49 +0500

    feat: splash page added

--- src/Body.tsx
+++ src/Body.tsx
@@ -2,10 +2,13 @@ import { Outlet } from "react-router-dom";
 import { NavBar } from "./components/NavBar/NavBar";
 import { FC } from "react";
 import { Footer } from "./components/Footer/Footer";
+import { SplashPage } from "./pages/Splash/Splash";
 
 export const Body: FC = () => {
+  const pathname = window.location.pathname;
   return (
     <>
+      {pathname === "/" && <SplashPage />}
       <NavBar />
       <Outlet />
       <Footer />
---
+++ src/pages/Splash/Splash.tsx
@@ -0,0 +1,24 @@
+import { FC, useState } from "react";
+import styles from "./Splash.module.scss";
+import { Button } from "src/components/Button/Button";
+import logo from "src/assets/images/logo.png";
+
+interface IProps {}
+
+export const SplashPage: FC<IProps> = () => {
+  const [hide, setHide] = useState(false);
+  return (
+    <div className={`${styles.container} ${hide && styles.hide}`}>
+      <div className={styles.leftSide}>
+        <img className={styles.logo} src={logo} alt="" />
+        <p className={styles.text}>
+          Looking to be part of my World Triangle community of traders. I’m here to share my knowledge, insights,
+          experience to enhance traders skills. Also to lend my edge to help traders progress towards success.{" "}
+        </p>
+        <Button className={styles.button} onClick={() => setHide(true)}>
+          Join the community now!
+        </Button>
+      </div>
+    </div>
+  );
+};
