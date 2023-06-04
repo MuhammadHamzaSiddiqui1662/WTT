ad5547da724ae400f72d4fb32b2a9e7fa88a2c14
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-06-05 02:47:15 +0500

    fix: home's get started button redirected to contact us

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,33 +9,17 @@ export const Home: FC = () => {
     <Page>
       <div className={styles.container}>
         <div className={styles.rightSide}>
-          <h1
-            className={styles.heading}
-            data-aos="fade-right"
-            data-aos-duration="1000"
-            // data-aos-anchor-placement="top-bottom"
-          >
+          <h1 className={styles.heading} data-aos="fade-right" data-aos-duration="1000">
             Bringing your trading experience to a higher level, using my edge.
           </h1>
-          <p
-            className={styles.description}
-            data-aos="fade-left"
-            data-aos-delay="200"
-            data-aos-duration="1000"
-            // data-aos-anchor-placement="top-bottom"
-          >
+          <p className={styles.description} data-aos="fade-left" data-aos-delay="200" data-aos-duration="1000">
             Trading is a challenging field, and having an edge can make all the difference. Stay one step ahead of the
             game by using proven approach and insights to navigate the dynamic world of trading. Let my edge sharpen
             your decision-making process, increase your profit potential, and pave the way to success. Don't just trade,
             stand out with this edge that I am willing to share.
           </p>
-          <Link to="/notes">
-            <Button
-              data-aos="fade-up"
-              data-aos-delay="200"
-              data-aos-duration="1000"
-              // data-aos-anchor-placement="top-bottom"
-            >
+          <Link to="/contact-us">
+            <Button data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
               Get Started
             </Button>
           </Link>
