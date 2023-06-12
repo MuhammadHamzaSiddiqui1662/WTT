908b7ab64356ff3f17f600c472d0ca9470461109
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-06-12 21:34:16 +0500

    feat: new translusant loader created for in app loading

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,15 +1,9 @@
-import React, { useEffect, useState } from "react";
 import styles from "./Loader.module.scss";
 import { FaSpinner } from "react-icons/fa";
 
 export const Loader = () => {
-  const [hide, setHide] = useState(false);
-  useEffect(() => {
-    setTimeout(() => setHide(true), 1500);
-  }, []);
-
   return (
-    <div className={`${styles.container} ${hide && styles.hide}`}>
+    <div className={styles.container}>
       <FaSpinner size={40} className={styles.spinner} />
     </div>
   );
