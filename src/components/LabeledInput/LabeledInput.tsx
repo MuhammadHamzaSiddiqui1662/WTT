d1cc45658902285a677888cd2b57effc4e613027
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2023-06-15 01:08:20 +0500

    feat: required indicator added

--- src/components/LabeledInput/LabeledInput.tsx
+++ src/components/LabeledInput/LabeledInput.tsx
@@ -13,13 +13,21 @@ interface IProps {
   error?: string;
 }
 
-export const LabeledInput: FC<IProps> = ({ label, name, error, ...props }) => {
+export const LabeledInput: FC<IProps> = ({ label, name, error, required = false, ...props }) => {
   return (
     <div className={styles.container}>
       <label className={styles.label} htmlFor={name || label}>
+        {required && "*"}
         {label}:
       </label>
-      <input className={styles.input} type="text" id={name || label} name={name || label} {...props} />
+      <input
+        className={styles.input}
+        type="text"
+        id={name || label}
+        name={name || label}
+        required={required}
+        {...props}
+      />
       {error && <p className={styles.error}>{error}</p>}
     </div>
   );
